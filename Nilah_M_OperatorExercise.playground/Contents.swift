5c975f66a51f1ee7aeb7765cd7fd50510c26553b
nilah-m2021 <67973031+nilah-m2021@users.noreply.github.com>
2020-07-14 12:20:24 -0500

    1st Commit
    1st Commit

---
+++ Nilah_M_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+//Top Section
+let valueZero = 0
+let valueOne = 1
+let valueTwo = 2
+let valueThree = 3
+let valueFour = 4
+let valueFive = 5
+let valueSix = 6
+let valueSeven = 7
+let valueEight = 8
+let valueNine = 9
+
+//Middle Section
+let addition = valueFive + valueTwo
+let subtraction = valueOne - valueSeven
+let multiplication = valueFour * valueSix
+let divison = valueNine / valueThree
+
+//Bottom Section
+print("Addition: " + String(addition))
+print("Subtraction: " + String(subtraction))
+print("Multiplication: " + String(multiplication))
+print("Division: " + String(divison))
